55b5a67e23dec98b72578fd96f65e17616b1a802
Luong1234 <antimage0210@gmail.com>
2015-11-10 14:19:18 +0700

    commit 9

--- KetNoiDB/KetNoi.cs
+++ KetNoiDB/KetNoi.cs
@@ -14,7 +14,7 @@ namespace KetNoiDB
 
         public static String connect()
         {
-            return (@"Data Source=ChiConCaiTen\SQLEXPRESS;Initial Catalog=QLCT_2;Integrated Security=true;");
+            return (@"Data Source=PhamVANLUONG\SQLEXPRESS;Initial Catalog=QLCT_2;Integrated Security=true;");
         }
     }
 }
