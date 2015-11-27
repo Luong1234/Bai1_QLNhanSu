5edb8bae1211473be61bfc7e357ec173ea3ae357
dungtienmta <dungtienmta@gmail.com>
2015-11-27 14:18:55 +0700

    ddd

--- Bai1_QLNhanSu/Form1.cs
+++ Bai1_QLNhanSu/Form1.cs
@@ -47,8 +47,22 @@ namespace Bai1_QLNhanSu
             dt = nhanvien.HienThiNhanVien();
             dgvNhanVien.DataSource = dt;
             KhoaDieuKhien();
+            KhoiTao();
         }
-
+        public void KhoiTao()
+        {
+            txtMaNV.Text = dgvNhanVien.Rows[0].Cells[0].Value.ToString();
+            txtHoDem.Text = dgvNhanVien.Rows[0].Cells[1].Value.ToString();
+            txtTenNV.Text = dgvNhanVien.Rows[0].Cells[2].Value.ToString();
+            txtGT.Text = dgvNhanVien.Rows[0].Cells[4].Value.ToString();
+            dtpNgaySinh.Value = DateTime.Parse(dgvNhanVien.Rows[0].Cells[3].Value.ToString());
+            txtLuong.Text = dgvNhanVien.Rows[0].Cells[5].Value.ToString();
+            txtDiaChi.Text = dgvNhanVien.Rows[0].Cells[6].Value.ToString();
+            cbMa_NQL.Text = dgvNhanVien.Rows[0].Cells[7].Value.ToString();
+            cbMaDV.Text = dgvNhanVien.Rows[0].Cells[8].Value.ToString();
+            txtChucVu.Text = dgvNhanVien.Rows[0].Cells[9].Value.ToString();
+            txtSDT.Text = dgvNhanVien.Rows[0].Cells[10].Value.ToString();
+        }
         private void btnThem_Click(object sender, EventArgs e)
         {
             MoDieuKhien();
